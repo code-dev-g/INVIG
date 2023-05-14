307e34d5e4d1cd9157607478ba648d28ecd7c6a3
code-dev-g <code.dev.g@gmail.com>
2023-05-14 19:29:50 +0530

    Fixed already signed in

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { authHandle, storeHandle } from "../config/firebase";
-import { signInWithEmailAndPassword, signOut, sendPasswordResetEmail } from "firebase/auth";
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { useRouter } from 'next/router';
 import { Box, Typography, Button, Select, MenuItem, TextField, Container } from "@mui/material";
 import { useFormik } from "formik";
@@ -15,6 +15,8 @@ import BrandCard from "../components/BrandCard";
 const LoginForm = () => {
 
     const router = useRouter();
+
+    const user = authHandle.currentUser;
 
     const formik = useFormik( {
         initialValues: {
@@ -75,80 +77,111 @@ const LoginForm = () => {
         },
     } );
 
-    return (
-        <>
-            <Head>
-                <title>Sign In - INVIG</title>
-            </Head>
-            <Container
+    if ( user ) {
+        return (
+            <Box
                 sx={ {
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    minHeight: "80vh",
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    minHeight: '80vh',
                 } }
             >
-                <BrandCard />
-                <form onSubmit={ formik.handleSubmit } style={ { display: "flex", flexDirection: "column", alignItems: "center", margin: "1rem" } }>
-                    <Typography variant="h3" style={ { marginBottom: "16px" } }>Sign In</Typography>
-                    <TextField
-                        id="email"
-                        name="email"
-                        label="Email address"
-                        variant="outlined"
-                        style={ { marginBottom: "18px", width: "300px" } }
-                        value={ formik.values.email }
-                        onChange={ formik.handleChange }
-                        onBlur={ formik.handleBlur }
-                        error={ formik.touched.email && Boolean( formik.errors.email ) }
-                        helperText={ formik.touched.email && formik.errors.email }
-                    />
-                    <TextField
-                        id="password"
-                        name="password"
-                        label="Password"
-                        type="password"
-                        variant="outlined"
-                        style={ { marginBottom: "18px", width: "300px" } }
-                        value={ formik.values.password }
-                        onChange={ formik.handleChange }
-                        onBlur={ formik.handleBlur }
-                        error={ formik.touched.password && Boolean( formik.errors.password ) }
-                        helperText={ formik.touched.password && formik.errors.password }
-                    />
-                    <Select
-                        id="type"
-                        name="type"
-                        style={ { marginBottom: "18px", width: "300px" } }
-                        value={ formik.values.type }
-                        onChange={ formik.handleChange }
-                    >
-                        <MenuItem value="faculty">Faculty</MenuItem>
-                        <MenuItem value="admin">Admin</MenuItem>
-                    </Select>
-                    <Button
-                        type="submit"
-                        variant="contained"
-                        color="primary"
-                        style={ { marginBottom: "18px" } }
-                        disabled={ formik.isSubmitting }
-                    >
-                        Sign In
-                    </Button>
-                    <Button
-                        type="button"
-                        variant="outlined"
-                        onClick={ () => {
-                            router.push( '/forgotpassword' );
-                        } }
-                    >
-                        Forgot password?
-                    </Button>
-                </form>
-            </Container>
-        </>
-    );
+                <Typography variant="h5">
+                    Already signed in as { user.email }
+                </Typography>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={ () => {
+                        signOut( authHandle );
+                        router.push( '/' );
+                    } }
+                    sx={ {
+                        margin: '1rem',
+                    }}
+                >
+                    Sign Out
+                </Button>
+            </Box>
+        );
+    } else {
+        return (
+            <>
+                <Head>
+                    <title>Sign In - INVIG</title>
+                </Head>
+                <Container
+                    sx={ {
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        minHeight: "80vh",
+                    } }
+                >
+                    <BrandCard />
+                    <form onSubmit={ formik.handleSubmit } style={ { display: "flex", flexDirection: "column", alignItems: "center", margin: "1rem" } }>
+                        <Typography variant="h3" style={ { marginBottom: "16px" } }>Sign In</Typography>
+                        <TextField
+                            id="email"
+                            name="email"
+                            label="Email address"
+                            variant="outlined"
+                            style={ { marginBottom: "18px", width: "300px" } }
+                            value={ formik.values.email }
+                            onChange={ formik.handleChange }
+                            onBlur={ formik.handleBlur }
+                            error={ formik.touched.email && Boolean( formik.errors.email ) }
+                            helperText={ formik.touched.email && formik.errors.email }
+                        />
+                        <TextField
+                            id="password"
+                            name="password"
+                            label="Password"
+                            type="password"
+                            variant="outlined"
+                            style={ { marginBottom: "18px", width: "300px" } }
+                            value={ formik.values.password }
+                            onChange={ formik.handleChange }
+                            onBlur={ formik.handleBlur }
+                            error={ formik.touched.password && Boolean( formik.errors.password ) }
+                            helperText={ formik.touched.password && formik.errors.password }
+                        />
+                        <Select
+                            id="type"
+                            name="type"
+                            style={ { marginBottom: "18px", width: "300px" } }
+                            value={ formik.values.type }
+                            onChange={ formik.handleChange }
+                        >
+                            <MenuItem value="faculty">Faculty</MenuItem>
+                            <MenuItem value="admin">Admin</MenuItem>
+                        </Select>
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            style={ { marginBottom: "18px" } }
+                            disabled={ formik.isSubmitting }
+                        >
+                            Sign In
+                        </Button>
+                        <Button
+                            type="button"
+                            variant="outlined"
+                            onClick={ () => {
+                                router.push( '/forgotpassword' );
+                            } }
+                        >
+                            Forgot password?
+                        </Button>
+                    </form>
+                </Container>
+            </>
+        );
+    }
 };
 
 export default LoginForm;
